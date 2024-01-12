551d6e5c25891625efc9dd74f03f1618bfa175a0
Lux <luxprajapati81@gmail.com>
2024-01-12 19:53:48 +0530

    Updated Pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -2,14 +2,19 @@ import React, { useContext } from "react";
 import { AppContext } from "../context/AppContext";
 
 const Pagination = () => {
-  const { page, nextPageHandler, totalPages, darkMode } = useContext(AppContext);
+  const { page, nextPageHandler, totalPages, darkMode } =
+    useContext(AppContext);
   return (
-    <div className={`fixed bottom-0 inset-x-0 bg-white py-2 border-t-2 font-serif border-t-gray-300
-     w-full ${darkMode ? "bg-slate-900" :"bg-white"}` }>
+    <div
+      className={`fixed bottom-0 inset-x-0 py-2 border-t-2 font-serif border-t-gray-300
+     w-full ${darkMode ? "bg-slate-900" : "bg-white"}`}
+    >
       <div className="flex items-center gap-x-3 w-11/12 max-w-2xl mx-auto">
         {page > 1 && (
           <button
-            className={`border-2 border-gray-300 py-1 px-4 rounded-md ${darkMode ? "text-white" : "text-black"}`}
+            className={`border-2 border-gray-300 py-1 px-4 rounded-md ${
+              darkMode ? "text-white" : "text-black"
+            }`}
             onClick={() => nextPageHandler(page - 1)}
           >
             Previous
@@ -17,14 +22,20 @@ const Pagination = () => {
         )}
         {page < totalPages && (
           <button
-            className={`border-2 border-gray-300 py-1 px-4 rounded-md ${darkMode ? "text-white" : "text-black"}`}
+            className={`border-2 border-gray-300 py-1 px-4 rounded-md ${
+              darkMode ? "text-white" : "text-black"
+            }`}
             onClick={() => nextPageHandler(page + 1)}
           >
             Next
           </button>
         )}
 
-        <p className={`text-sm font-semibold ml-auto ${darkMode? "text-white" : "text-black"}`}>
+        <p
+          className={`text-sm font-semibold ml-auto ${
+            darkMode ? "text-white" : "text-black"
+          }`}
+        >
           Page {page} of {totalPages}
         </p>
       </div>
